fd05d48b7d73197b8048be4adce314cf95ee9724
Pukaty-LR <lukas.roun@seznam.cz>
2025-07-08 18:51:50 +0200

    Finalni oprava server.js

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const http = require('http');
-const path = 'path'; //správně je const path = require('path');, ale tohle je jednodušší pro začátek
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const PORT = 3000;
 
-// Řekneme serveru, aby servíroval statické soubory z aktuálního adresáře
+// Řekneme serveru, aby posílal soubory z aktuálního adresáře (kde je hra.html)
 app.use(express.static(__dirname));
 
+// Hlavní cesta, která pošle soubor s hrou
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'hra.html'));
 });
 
 server.listen(PORT, () => {
-  console.log(`Server běží na portu ${PORT}. Hra je dostupná na http://localhost:3000`);
+  console.log(`Server běží! Otevři v prohlížeči http://TVOJE_IP:${PORT}`);
 });
